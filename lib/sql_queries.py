eda21997980478c62b15abad9e05003a0098a1af
Levin Mwanganyi Mwambi <artmwanganyi09@outlook.com>
2023-12-10 13:04:16 +0300

    Assignment completed

--- lib/sql_queries.py
+++ lib/sql_queries.py
@@ -7,12 +7,12 @@ select_all_bears_names_and_orders_in_alphabetical_order = """
 """
 
 select_all_bears_names_and_ages_that_are_alive_and_order_youngest_to_oldest = """
-    SELECT name, age, FROM bears WHERE alive = '1' ORDER BY age ASC
+    SELECT name, age FROM bears WHERE alive = 1 ORDER BY age ASC
 """
 
 select_oldest_bear_and_returns_name_and_age = """
-    Write your SQL query here
+    SELECT name, age FROM bears ORDER BY age DESC LIMIT 1
 """
 select_youngest_bear_and_returns_name_and_age = """
-    Write your SQL query here
+    SELECT name, age FROM bears ORDER BY age ASC LIMIT 1
 """
